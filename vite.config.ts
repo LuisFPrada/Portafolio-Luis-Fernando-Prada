ca0a72afca0a0d173fbccd712ee1a92777666047
Luis FPrada <luifernando1997@hotmail.com>
2025-06-11 06:23:13 -0500

    Merge branch 'master' of https://github.com/LuisFPrada/Portafolio-Luis-Fernando-Prada

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
-});
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html' 
+    }
+  }
+
+}});
